ccc00bef5efdd252e5de3810959d3b3f327c4a7c
Nicoisonline <doldinicolas@orange.fr>
2024-10-07 16:25:05 +0200

    :memo: MAJ OK

--- TP/__main__.py
+++ TP/__main__.py
@@ -1,48 +1,39 @@
 from TP.loading import load_directory
 from TP.kmers import stream_kmers, kmer2str
 
-def create_index(text, k):
-    """ Create a kmer index from a text"""
+
+def compare_kmers(km1,km2):
+    for i in range(len(km1)):
+        if km1[i]!=km2[i]:
+            return False
+    return True
+
+def create_index(file, k):
     index = {}
-    # --- To complete ---
-    for kmer in stream_kmers(text, k):
+    for idx,km in enumerate(stream_kmers(file,k)):
+        kmer = kmer2str(km,k)
         if kmer in index:
             index[kmer] += 1
-        else:
+        else :
             index[kmer] = 1
     return index
 
-def compare_sequences(seq1, seq2, k): 
-    index = create_index(seq1, k)
-    intersection = 0
+def jaccard(fileA, fileB, k):
+    jac = 0
+    index = create_index(fileA, k)
+    intersect = 0
     union = sum(index.values())
-    for kmer in stream_kmers(seq2, k):
-        if kmer in index:
-            intersection += 1
-            union += index[kmer] - 1
+    for idx,kme in enumerate(stream_kmers(fileB, k)):
+        kmer2 = kmer2str(kme,k)
+        if (kmer2 in index) and (index[kmer2]>0):
+            intersect += 1
+            index[kmer2] -= 1
         else:
             union += 1
-
-def jaccard(fileA, fileB, k):
-    """ Compute the Jaccard similarity between two files"""
-    j = 0
-    # --- To complete ---
-    indexA = create_index(fileA, k)
-    indexB = create_index(fileB, k)
-    intersection = 0
-    union = 0
-    for kmer in indexA:
-        if kmer in indexB:
-            intersection += 1
-            union += max(indexA[kmer], indexB[kmer])
-        else:
-            union += indexA[kmer]
-    for kmer in indexB:
-        if kmer not in indexA:
-            union += indexB[kmer]
-    j = intersection / union
-    return j
-
+    if union == 0 :
+        return 0
+    jac = intersect/union
+    return jac
 
 
 if __name__ == "__main__":
@@ -51,13 +42,11 @@ if __name__ == "__main__":
     # Load all the files in a dictionary
     files = load_directory("data")
     k = 21
-    
+
     print("Computing Jaccard similarity for all pairs of samples")
     filenames = list(files.keys())
+
     for i in range(len(files)):
         for j in range(i+1, len(files)):
-            
-            # --- Complete here ---
-            
-            j = jaccard(files[filenames[i]], files[filenames[j]], k)
-            print(filenames[i], filenames[j], j)
+            jac = jaccard(files[filenames[i]], files[filenames[j]], k)
+            print(filenames[i], filenames[j], jac)
--- TP/kmers.py
+++ TP/kmers.py
@@ -14,18 +14,34 @@ def kmer2str(val, k):
     str_val.reverse()
     return "".join(str_val)
 
+def encode_kmer(text, k):
+    kmer = 0
+    kmer_inv = 0
+    for letter in text[0:k]:
+        kmer_inv<<=2 
+        kmer<<=2
+        kmer = kmer + encode_nucl(letter)
+        kmer_inv = kmer_inv + rev_nuc(encode_nucl(letter))
+    return kmer, kmer_inv
 
 def stream_kmers(text, k):
-    """Aussi appelé enumerate_kmers"""
-    # --- To complete ---
-    mask = (1 << (2*(k-1))) - 1
-    kmer = text[0:k-1]
-    for i in range(len(text) - (k - 1)):
-        yield kmer
+    """enumerate_kmer"""
+    text = text[0]
+    mask = (1<<(2*(k-1)))-1
+    kmer, kmer_inv = encode_kmer(text,k)
+    for i in range(len(text)-(k)):
+        yield min(kmer, kmer_inv)
         kmer &= mask
+        kmer_inv &= mask
         kmer <<= 2
-        kmer += encode_nucl(text[i + k])
+        kmer_inv <<= 2
+        kmer = kmer + encode_nucl(text[i+k])
+        kmer_inv = kmer_inv + rev_nuc(encode_nucl(text[i+k]))
+    yield min(kmer, kmer_inv)
 
-def encode_nucl(nucl):
-    """ Encode a nucleotide into a 2-bits integer"""
-    return {'A': 0, 'C': 1, 'T': 2, 'G': 3}[nucl]
+def encode_nucl(letter):
+    encoding = {"A" : 0, "C" : 1, "T" : 2, "G" : 3}
+    return encoding[letter]
+
+def rev_nuc(n_int) :
+    return (n_int + 2) & 3
